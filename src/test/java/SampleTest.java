22e27a5a4520ed534bf92a72a67008c919ca0456
Rajat Srivastava <rajat.srivastava1@swiggy.in>
2022-08-05 01:11:26 +0530

    Added create user test case

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -16,6 +17,26 @@ public class SampleTest {
 
 
 
+
+    }
+
+    @Test
+    public void shouldCreateUsers()
+    {
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer 92d624d4b09559bc146b2926abccdc10cd42ef32460bcf51e51091b5bce762a1")
+                .body("{\n" +
+                        "  \"name\": \"Tenali Ramakrishna\",\n" +
+                        "  \"gender\": \"male\",\n" +
+                        "  \"email\": \"tenali.ramakrishna1@15ce.com\",\n" +
+                        "  \"status\": \"active\"\n" +
+                        "}")
+                .when()
+                    .post("https://gorest.co.in/public/v2/users")
+                .then()
+                    .log().body();
 
     }
 }
