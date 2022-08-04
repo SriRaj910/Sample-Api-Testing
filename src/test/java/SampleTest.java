2d4e39f737080666d7b88792c1282ebc15646f35
Rajat Srivastava <rajat.srivastava1@swiggy.in>
2022-08-04 23:04:25 +0530

    Added Get user test case with rest assured dependency

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,10 +1,21 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class SampleTest {
     @Test
-    public void shouldTestSomething()
+    public void shouldGetAllUsers()
     {
-        Assert.assertEquals(1,1);
+        given()
+                .when()
+                    .get( "https://gorest.co.in/public/v2/users")
+                .then()
+                    .statusCode(200)
+                    .log().body();
+
+
+
+
     }
 }
